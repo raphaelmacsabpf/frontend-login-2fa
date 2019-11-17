42830cde095ccbffa4cb223cba8991cb577e63bc
Raphael Santos <raphaelmacsa@gmail.com>
2019-11-17 21:39:07 -0200

    Add login page controls and style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 import Routes from './routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-const App = () => <Routes />;
+const App = () => 
+<div className="app">
+  <div className="">
+    <div className="menu app-navbar">
+      <a className="menu-item" href="/">Home</a>
+      <a className="menu-item" href="/a-empresa">A Empresa</a>
+      <a className="menu-item" href="/trabalhe-conosco">Trabalhe Conosco</a>
+      <a className="menu-item" href="/solucoes">Soluções</a>
+      <a className="menu-item" href="/contato">Contato</a>
+      <div className="sign-in">
+        <a className="internal" href="/login">Entrar</a>
+      </div>
+    </div>
+  </div>
+  <Routes />
+  <div className="footer">
+
+  </div>
+</div>;
 
 export default App;
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,34 @@
 import React, { Component} from 'react';
+import { Button, Form, ButtonToolbar } from 'react-bootstrap';
 import './login.css';
 
 class Login extends Component {
     render() {
         return (
-            <div className="login-page">
-                <h1>Tela de Login!</h1>
+            <div>
+                <div className="container">
+                    <div className="row login-page">                        
+                        <div className="mx-auto col-md-5 col-sm-8 login-form">
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" className="form-input" />
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Senha</Form.Label>
+                                    <Form.Control type="password" className="form-input" />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicLink">
+                                    <Button variant="link" className="forgot-password">Esqueci minha senha</Button>
+                                </Form.Group>
+                                <div className="sign-in">
+                                    <a className="internal" href="/login">Entrar</a>
+                                </div>
+                            </Form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
